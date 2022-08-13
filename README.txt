f4f75bdb5feffb7ead9b5a2a824647303aa77e58
Emir <emir210515@ogr.duzce.edu.tr>
2022-08-13 13:00:55 +0300

    initial-commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+Dönüştürülmesini istediğiniz locale_game'i -> locale_game.txt olarak
+Yazıların alınmasını istediğiniz locale_game'i -> locale_game_tr.txt olarak aynı klasöre koyun ve çalıştırın
---
+++ translate.py
@@ -0,0 +1,30 @@
+import os, sys
+
+tempStr = ""
+
+
+
+try:
+    game = open("locale_game.txt", "r")
+    newdoc = open("newdoc.txt", "w")
+    log = open("log.txt", "w")
+except:
+    os.system("PAUSE")
+    sys.exit()
+
+for i in game.readlines():
+    tempStr = i.split("\t")[0]
+    game_tr = open("locale_game_tr.txt", "r")
+    for j in game_tr.readlines():
+        #print(tempStr,j.split("\t")[0])
+        if tempStr == j.split("\t")[0]:
+            # print(tempStr)
+            tempStr += " " + j.split("\t")[1]
+            newdoc.writelines(tempStr)
+            break
+    else:
+        log.writelines("{x}".format(x = tempStr) + "\t\n")
+
+    game_tr.close()
+newdoc.close()
+game.close()
